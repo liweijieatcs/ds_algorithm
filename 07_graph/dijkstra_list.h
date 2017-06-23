b27e63611856c6939d0c79aee05914e584042645
liweijie <ee.liweijie@gmail.com>
2017-06-23 10:49:52 +0800

    use linkedlist and matirix graph implement shortest path

---
+++ 07_graph/dijkstra_list.h
@@ -0,0 +1,6 @@
+#ifndef _DIJKSTRA_LIST_H_
+#define _DIJKSTRA_LIST_H_
+
+int list_dijkstra_test(void);
+
+#endif
---
+++ 07_graph/dijkstra_matrix.h
@@ -0,0 +1,6 @@
+#ifndef _DIJKSTRA_MATRIX_H_
+#define _DIJKSTRA_MATRIX_H_
+
+int matrix_dijkstra_test(void);
+
+#endif
